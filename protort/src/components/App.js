95ebf9473e8ec636a05f7adbc079792ff813e5d6
MariaMantusova <maria.mantusova@mail.ru>
2023-02-03 19:05:27 +0300

    feat: add structure

--- protort/src/components/App.js
+++ protort/src/components/App.js
@@ -1,9 +1,33 @@
+const strawberry = require('../images/strawberry-sale.png');
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    return (
+        <div className="App">
+            <div className="scroll-container">
+                <div className="scroll-container__first-part">
+                    <img className="scroll-container__image" src={strawberry} alt="Клубника"/>
+                    <p className="scroll-container__text">Скидка 10% на первый заказ</p>
+                    <img className="scroll-container__image" src={strawberry} alt="Клубника"/>
+                    <p className="scroll-container__text">Скидка 10% на первый заказ</p>
+                    <img className="scroll-container__image" src={strawberry} alt="Клубника"/>
+                    <p className="scroll-container__text">Скидка 10% на первый заказ</p>
+                    <img className="scroll-container__image" src={strawberry} alt="Клубника"/>
+                    <p className="scroll-container__text">Скидка 10% на первый заказ</p>
+                </div>
+                <div className="scroll-container__second-part">
+                    <img className="scroll-container__image" src={strawberry} alt="Клубника"/>
+                    <p className="scroll-container__text">Скидка 10% на первый заказ</p>
+                    <img className="scroll-container__image" src={strawberry} alt="Клубника"/>
+                    <p className="scroll-container__text">Скидка 10% на первый заказ</p>
+                    <img className="scroll-container__image" src={strawberry} alt="Клубника"/>
+                    <p className="scroll-container__text">Скидка 10% на первый заказ</p>
+                    <img className="scroll-container__image" src={strawberry} alt="Клубника"/>
+                    <p className="scroll-container__text">Скидка 10% на первый заказ</p>
+                </div>
+            </div>
+            <header className="App-header">
+            </header>
+        </div>
   );
 }
 
