eb218c5fd33a9d32c56f526d70e598c2ca1c8ef8
MariaMantusova <maria.mantusova@mail.ru>
2023-02-04 15:45:48 +0300

    feat: add header without logo

--- protort/src/components/App.js
+++ protort/src/components/App.js
@@ -2,13 +2,19 @@ import Marquee from "react-fast-marquee";
 
 function App() {
     return (
-        <div className="App">
-            <Marquee className="scroll-container" direction="right">
-                Скидка на первый заказ 10%&nbsp;&nbsp;&nbsp;Скидка на первый заказ 10%&nbsp;&nbsp;&nbsp;Скидка на первый
-                заказ 10%&nbsp;&nbsp;&nbsp;Скидка на первый заказ 10%&nbsp;&nbsp;&nbsp;Скидка на первый заказ
-                10%&nbsp;&nbsp;&nbsp;Скидка на первый заказ 10%&nbsp;&nbsp;&nbsp;Скидка на первый заказ
-                10%&nbsp;&nbsp;&nbsp;
+        <div className="page">
+            <Marquee className="scroll-container" direction="right" gradient={false}>
+                Скидка на первый заказ 10%&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Скидка на первый заказ 10%&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Скидка на первый
+                заказ 10%&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Скидка на первый заказ 10%&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Скидка на первый заказ
+                10%&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Скидка на первый заказ 10%&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             </Marquee>
+            <header className="header">
+                <nav className="header__nav">
+                    <a className="header__link" href="#">Каталог</a>
+                    <a className="header__link" href="#">Заказ</a>
+                    <a className="header__link" href="#">О нас</a>
+                </nav>
+            </header>
         </div>
     );
 }
