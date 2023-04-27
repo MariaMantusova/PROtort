1798a35fc0f2a3f533ecd3f9861335ae5b420643
MariaMantusova <maria.mantusova@mail.ru>
2023-04-27 17:11:06 +0300

    feat: add main image block

--- protort/src/components/Main/Main.js
+++ protort/src/components/Main/Main.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../Header/Header";
 import ScrollingLine from "../ScrollingLine/ScrollingLine";
+import ImageBlock from "../ImageBlock/ImageBlock";
 
 function Main() {
     return (
         <>
             <ScrollingLine/>
             <Header/>
+            <ImageBlock/>
         </>
     );
 }
